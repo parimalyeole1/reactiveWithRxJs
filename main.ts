f7f770d7e6ec2f13444f1df6f7bc56c37d914754
Parimal Yeole <parimal.yeole1@gmail.com>
2018-02-10 19:34:14 +0530

    normal way of creating objeserver in typescript

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-alert("Okey!");
+import { Observable, Observer } from "rxjs";
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const source = Observable.from(numbers);
+
+class MyObserver implements Observer<number> {
+ 
+  next(value: any) {
+    console.info(`value is ${value}`);
+  }
+
+  error(e: any) {
+    console.error(`value is ${e}`);
+  }
+
+  complete() {
+    console.log(`completed`);
+  }
+}
+
+source.subscribe(new MyObserver());
+source.subscribe(new MyObserver());
