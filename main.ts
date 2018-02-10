adca28099bd2d7d5c2b9618db9dc6e19c381eab6
Parimal Yeole <parimal.yeole1@gmail.com>
2018-02-10 19:38:06 +0530

    easier way to to subscribe

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ const numbers = [1, 2, 3, 4, 5, 6];
 const source = Observable.from(numbers);
 
 class MyObserver implements Observer<number> {
- 
   next(value: any) {
     console.info(`value is ${value}`);
   }
@@ -18,5 +17,9 @@ class MyObserver implements Observer<number> {
   }
 }
 
-source.subscribe(new MyObserver());
-source.subscribe(new MyObserver());
+// easier way to to subscribe
+source.subscribe(
+  (value: any) => console.info(`value is ${value}`),
+  (e: any) => console.error(`value is ${e}`),
+  () => console.log(`completed`)
+);
